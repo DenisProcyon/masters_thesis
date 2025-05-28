5378bed33ad7bed0ce1a833accfbd7fd9ffe4471
DenisProcyon <denis.shadrin@procyon.ee>
2025-05-28 13:12:48 +0200

    google trends 21y

--- src/serp_google_trends.py
+++ src/serp_google_trends.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 MONGO_IP = os.getenv("MONGO_IP")
 MONGO_PORT = os.getenv("MONGO_PORT")
-MONGO_DB = "serpapi"
+MONGO_DB = "serpapi_5y"
 MONGO_USERNAME = os.getenv("MONGO_USERNAME")
 MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
 SERPAPI_API_KEY = os.getenv("SERPAPI_API_KEY")
@@ -78,18 +78,31 @@ def main():
     ]
 
     for index_k, keyword_set in enumerate(keywords):
-        # params_geo = {
-        #     "engine": "google_trends",
-        #     "q": keyword_set,
-        #     "data_type": "GEO_MAP",
-        #     "hl": "es",
-        #     "geo": "MX",
-        #     "region": "REGION",
-        #     "api_key": SERPAPI_API_KEY
-        # }
+        params_geo = {
+            "engine": "google_trends",
+            "q": keyword_set,
+            "data_type": "GEO_MAP",
+            "hl": "es",
+            "geo": "MX",
+            "date": "all",
+            "region": "REGION",
+            "api_key": SERPAPI_API_KEY
+        }
 
-        # geo_data = get_data(params_geo)
-        # save_data(params_geo, geo_data, "geo")
+        collection_name = f'serpapi_geo_{keyword_set}'
+        if collection_name in mongo_client.get_all_collections():
+            print(f'Collection {collection_name} already exists. Skipping...')
+
+            pass
+
+        try:
+            geo_data = get_data(params_geo)
+
+            save_data(params_geo, geo_data, "geo")
+        except Exception as e:
+            print(f'Error fetching geo data for keyword "{keyword_set}": {e}')
+
+            pass 
 
         for index, state in enumerate(STATES):
             params_timeseries = {
@@ -98,6 +111,7 @@ def main():
                 "data_type": "TIMESERIES",
                 "hl": "es",
                 "geo": state,
+                "date": "all",
                 "api_key": SERPAPI_API_KEY
             }
 
@@ -110,7 +124,7 @@ def main():
             try:
                 timeseries_data = get_data(params_timeseries)
             except Exception as e:
-                print(f'Error fetching data for state {state} with keyword "{keyword_set}": {e}')
+                print(f'Error fetching timeseries data for state {state} with keyword "{keyword_set}": {e}')
                 continue
 
             save_data(params_timeseries, timeseries_data, "timeseries", state)
